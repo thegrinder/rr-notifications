25d9dd3f313b8438ed2ddf7e85af46ae464abafc
Pawel Szczepkowski <szczepkowski@strands.com>
2017-06-20 10:38:19 +0200

    Remove polished as dependency

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,13 +1,19 @@
-import styled from 'styled-components';
-import { position } from 'polished';
+import styled, { css } from 'styled-components';
 import { array, string } from 'prop-types';
+
+const COORDINATES = ['top', 'right', 'bottom', 'left'];
+const getPosition = css`
+  ${props => props.position.map((value, index) => (
+    `${COORDINATES[index]}: ${value};`
+  ))}
+`;
 
 const Container = styled.div`
   position: fixed;
   display: flex;
   z-index: 999999;
   flex-direction: ${props => (props.stackNextOn === 'top' ? 'column-reverse' : 'column')};
-  ${props => position(...props.position)}
+  ${getPosition}
 `;
 
 Container.propTypes = {
