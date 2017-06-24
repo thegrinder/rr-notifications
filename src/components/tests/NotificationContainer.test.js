f9b945a96393fd6e17ddc6eb5d7f336240ff183d
pawel <paul.szczepkowski@gmail.com>
2017-06-24 21:11:29 +0200

    Add enzyme and test NotificationContainer

---
+++ src/components/tests/NotificationContainer.test.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { mount } from 'enzyme';
+import configureStore from 'redux-mock-store';
+
+import { setNotificationHeight, hideNotification } from '../../redux/actions';
+import NotificationContainer from '../NotificationContainer';
+
+jest.useFakeTimers();
+
+const mockStore = configureStore();
+
+describe('<NotificationContainer />', () => {
+  let renderedComponent;
+  let store;
+  const testProps = {
+    uid: 1,
+    dismissAfter: 1000,
+    animatedMargin: 'bottom',
+    animationDuration: '.4s',
+    animationEasing: 'ease',
+  };
+  const testInitialState = {
+    notifications: [
+      { uid: testProps.uid, isVisible: true, height: 20 },
+    ],
+  };
+
+  beforeEach(() => {
+    setTimeout.mockClear();
+    store = mockStore(testInitialState);
+    store.dispatch = jest.fn();
+    renderedComponent = mount(
+      <Provider store={store}>
+        <NotificationContainer {...testProps} />
+      </Provider>,
+    );
+  });
+
+  it('maps state and dispatch to props', () => {
+    expect(renderedComponent.find('NotificationContainer').props()).toMatchObject(testProps);
+  });
+
+  it('dispatches setNotificationHeight and then hideNotification', () => {
+    const { uid, dismissAfter } = testProps;
+    expect(store.dispatch).toHaveBeenCalledWith(setNotificationHeight(uid, 0));
+    jest.runAllTimers();
+    expect(setTimeout.mock.calls.length).toBe(1);
+    expect(setTimeout.mock.calls[0][1]).toBe(dismissAfter);
+    expect(store.dispatch).toHaveBeenCalledWith(hideNotification(uid));
+  });
+});
