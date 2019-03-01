3089a4183ca260cbf91a92c654b1f1be8b1e67a8
thegrinder <paul.szczepkowski@gmail.com>
2019-03-01 22:42:13 +0100

    test(NotificationContainer): test it

---
+++ src/components/NotificationContainer/__tests__/NotificationContainer.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { render, cleanup } from 'react-testing-library';
+import 'jest-dom/extend-expect';
+
+import NotificationContainer from '../NotificationContainer';
+
+const children = (<p>test</p>);
+const props = {
+  position: ['20px', '30px', 'auto', 'auto'],
+  isVisible: true,
+  animationDuration: 100,
+  animationEasing: 'ease',
+  slideFromSide: 'left',
+};
+
+const renderComponent = () => render(
+  <NotificationContainer {...props}>
+    {children}
+  </NotificationContainer>,
+);
+
+describe('<NotificationsContainer />', () => {
+  afterEach(cleanup);
+
+  it('should render correctly', () => {
+    const { container: { firstChild } } = renderComponent();
+    expect(firstChild).toBeDefined();
+    expect(firstChild).toMatchSnapshot();
+  });
+
+  it('should have children', () => {
+    const { container: { firstChild }, getByText } = renderComponent();
+    const childrenElement = getByText('test');
+    expect(firstChild).toContainElement(childrenElement);
+  });
+});
